2d9c2aa9bf79874e2a1ceb3827c7fef626eb1578
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-11-17 16:08:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0;
+    int opzione=0;
+    cin>>a>>b>>opzione;
+    switch(opzione){
+        case 0:
+            cout<<(a*b)/2<<endl;
+            break;
+        case 1:
+            cout<<pow(a,2)<<endl;
+            break;
+        case 2:
+            cout<<(a*b)<<endl;
+            break;
+        default:
+            cout<<"opzione non valida"<<endl;
+    }
+    return 0;
 }
+
